53501246a831234c684131b29a45b5067169a26b
GOLDEN-NO <74879703+GOLDEN-NO@users.noreply.github.com>
2021-05-22 14:52:18 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 var Farm,FarmBackground,marketBackground,market
 var dialogue1,dialogue2
 var gameState
+var pepperSeeds,flowerSeeds,pumpkinSeeds,tomatoSeeds
+var pepperSeedsImage,flowerSeedsImage,pumpkinSeedsImage,tomatoSeedsImages
 gameState = 0
 
 function preload(){
@@ -23,6 +25,7 @@ dialogue1.shapeColor = "black"
 dialogue2 = createSprite(windowWidth/2,100,600,150)
 dialogue2.shapeColor = "black"
 dialogue2.visible = false
+console.log(windowWidth);
 }
 function draw(){
     drawSprites()
@@ -42,10 +45,32 @@ if(gameState===1){
     text("MORE TEXT WOW",windowWidth/2-50,100)
 }
 if(gameState === 1&&mousePressedOver(Farm)){
-     market.visible = true
-     dialogue2.destroy()
-     Farm.visible = false  
-         gameState = 2
+    dialogue2.destroy()
+    Farm.visible = false  
+    market.visible = true
+     seeds()
+      if(mousePressedOver(pepperSeeds)||mousePressedOver(flowerSeeds)||mousePressedOver(pumpkinSeeds)||mousePressedOver(tomatoSeeds)){
+  pepperSeeds.visible=false
+    flowerSeeds.visible=false
+    pumpkinSeeds.visible=false;
+    tomatoSeeds.visible=false
+    buy.visible=false
+    gameState=2;
+           }}
 
+if(gameState===2){
+    market.visible=false;
+    
+    Farm.visible=true;
+dialogue3= createSprite(windowWidth/2,windowHeight/2,100,50);
+text("Are you sure you want to buy this?",windowWidth/2-100, windowHeight/2+25);
+yes = createSprite(windowWidth/2-50, windowHeight/2+50, 10,10);
+no = createSprite(windowWidth/2+50, windowHeight/2+50, 10,10);    
+}}       
+function seeds(){
+    pepperSeeds = createSprite(windowWidth*0.2,100,10,10)
+    flowerSeeds = createSprite(windowWidth*0.4 ,100,10,10)
+    pumpkinSeeds = createSprite(windowWidth*0.6 ,100,10,10)
+    tomatoSeeds = createSprite(windowWidth*0.8,100,10,10)
+    buy = createSprite(windowWidth/2, windowHeight/1.25,50,50)
 }
-}       
